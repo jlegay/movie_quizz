4bcaa9b221aa0d0d1a158c8b7f75a5d3f2814746
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-12-14 11:50:33 +0100

    Create moviequiz

---
+++ moviequiz.js
@@ -0,0 +1,11 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function buildQuiz(){}
+
+function showResults(){}
+
+bulidQuiz();
+
+submitButton.addEventListener('click', showResults);
